280fa0815c8bf0598f0335ac9cf589fd4211f56c
Thusithanjana <thusithanjana@gmail.com>
2020-02-24 18:05:41 +0530

    frst commit

---
+++ app/src/main/java/com/example/gitlab410right/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gitlab410right;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
